ef0c44a2135cda429ef73e584c2b60c90acd715a
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2022-11-25 17:01:24 -0300

    Create Crosshair.cpp

---
+++ Crosshair.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "stdafx.h"
+#include "Juego.h"
+using namespace sf;
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[]) {
+
+	Juego myGame;
+	myGame.Loop();
+
+	return 0;
+}
