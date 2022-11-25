fabeabadb7f65d491c2063b546212d1629d46dff
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2022-11-25 16:59:24 -0300

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,18 @@
+/*
+El stdafx.h es un archivo de inclusion estandar del sistema o archivos
+de inclusion especificos de un proyecto 
+*/
+
+#pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+#include <iostream>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
